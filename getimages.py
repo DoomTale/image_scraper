62ba45f283a856b45f7fb8d2eca2c0749717b151
DoomTale <dev.doomtale@gmail.com>
2022-03-17 01:48:21 +0200

    class rework

---
+++ getimages.py
@@ -0,0 +1,86 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin
+from tqdm import tqdm
+import pathlib
+
+
+class ImagesLinks:
+    def __init__(self, url):
+        self.links = []
+        self.url = url
+
+    def ping(self, url=''):
+        """
+        Checks if url is a valid URL.
+        """
+        if url == '':
+            url = self.url
+        parsed = urlparse(url)
+        return bool(parsed.netloc) and bool(parsed.scheme)
+
+    def get(self):
+        """
+         Get image from scrap
+         """
+        try:
+            response = requests.get(self.url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'lxml')
+                found_images = soup.find_all("img")
+                for img in tqdm(found_images, "URLs: "):
+                    img_url = img.attrs.get("src")
+                    if not img_url:
+                        continue
+                    img_url = urljoin(self.url, img_url)
+                    try:
+                        pos = img_url.index("?")
+                        img_url = img_url[:pos]
+                    except ValueError:
+                        pass
+                    if self.ping(img_url):
+                        self.links.append(img_url)
+        except (ValueError, Exception):
+            print('An exception occurred')
+
+
+class Images:
+    def __init__(self, url, img_urls):
+        self.url = url
+        self.img_urls = img_urls
+
+    def __get_dir_name(self):
+        """
+        Get dir name
+        """
+        parsed = urlparse(self.url)
+        line = ''.join(filter(str.isalnum, str(parsed.netloc)))
+        str_dir = 'images/' + line
+        return str_dir
+
+    def download_image(self):
+        """
+        Downloads an image
+        """
+        path_dir = self.__get_dir_name()
+        if not os.path.isdir(path_dir):
+            os.makedirs(path_dir)
+        for img_url in tqdm(self.img_urls, "Saving:"):
+            try:
+                filename = os.path.join(path_dir, img_url.split('/')[-1])
+                file_extension = pathlib.Path(filename).suffix
+                if file_extension == "":
+                    filename = str(filename) + '.png'
+                elif file_extension.lower().endswith(('.png', '.jpg', '.jpeg')):
+                    filename = str(filename).replace(file_extension, '.png')
+                else:
+                    return
+                response = requests.get(img_url, stream=True)
+                if response.status_code == 200:
+                    with open(filename, 'wb') as file:
+                        for chunk in response:
+                            file.write(chunk)
+            except (ValueError, Exception):
+                print('An exception occurred')
+
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from getimage import get_images
+from getimages import ImagesLinks, Images
 
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36 OPR/84.0.4316.31'
-}
-url = input('>')
-get_images(url, headers)
+url_link = input('>')
+links = ImagesLinks(url_link)
+if links.ping():
+    links.get()
+    images = Images(url_link, links.links)
+    images.download_image()
